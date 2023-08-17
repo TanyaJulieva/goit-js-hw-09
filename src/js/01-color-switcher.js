7f3f8e9118224a8e5ede1eec1bc8aef56cb08b9a
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2023-08-17 22:56:56 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const elements = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
 
+elements.startBtn.addEventListener('click', handlerStartClick);
+elements.stopBtn.addEventListener('click', handlerStopClick);
+
+elements.stopBtn.disabled = true;
+
+let changeColorId = null
+
+function handlerStartClick() {
+    elements.body.style.backgroundColor = getRandomHexColor();
+    elements.startBtn.disabled = true;
+    elements.stopBtn.disabled = false;
+    changeColorId = setInterval(() => {
+        elements.body.style.backgroundColor = getRandomHexColor();
+    },1000);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+  function handlerStopClick() {
+    clearInterval(changeColorId);
+    elements.stopBtn.disabled = true;
+    elements.startBtn.disabled = false;
+  }
